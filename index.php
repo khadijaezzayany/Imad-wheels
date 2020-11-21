0aaafb960c3ab67adbd8855656f29bd7214f50d9
khadijaezzayany <khadijaezzayany8@gmail.com>
2020-11-22 00:41:31 +0100

    -html

--- index.php
+++ index.php
@@ -8,6 +8,105 @@
 </head>
 
 <body>
+    <div class="heronav">
+        <nav>
+            <ul>
+                <li>Monday-Saturday 7:00AM - 6:00PM</li>
+                <li>2605 Hill Road, Woodbridge, VA 22192</li>
+                <li>06 -99 44 95 57</li>
+            </ul>
+        </nav>
+    </div>
+    <div class="header">
+        <div class="header__nav">
+            <p class="header__nav--logo"> Imad <span>Whells</span></p>
+            <ul>
+                <li>HOME</li>
+                <li>ABOUT US</li>
+                <li>SERVICES</li>
+                <li>CONTACTS</li>
+            </ul>
+            <h1 class="header__h1"> </h1>
+            <a href="">CONTACTS</a>
+        </div>
+    </div>
+
+    <div class="sectionwhells">
+
+        <div class="sectionwhells__tires">
+            <img src="dist/img/tires.png" alt="">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis velit esse reiciendis quisquam
+                dolores officiis quibusdam pariatur dolor earum est, temporibus voluptate laborum alias at. Odio,
+                accusamus. Amet, debitis ex?</p>
+        </div>
+
+        <div class="sectionwhells__wheels">
+            <img src="dist/img/wheel.png" alt="">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Et error officiis, doloribus explicabo eum sunt
+                alias est esse dolores cum modi eaque magnam ipsam, quidem rem quasi assumenda consequuntur! Quidem.</p>
+        </div>
+    </div>
+    <div class="about">
+        <div class="about__under">
+            <h1>ABOUT US</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus rerum minima atque sint quas ipsa
+                tenetur aliquid quaerat placeat culpa dolores asperiores optio amet doloremque perspiciatis perferendis,
+                labore corrupti ut?</p>
+            <a href="">Let's fix your wheels</a>
+            <img src="dist/img/aboutdes.png" alt="">
+        </div>
+    </div>
+    <div class="service">
+        <div class="service__cart1">
+            <img src="dist/img/service1" alt="">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A, praesentium maxime nobis laborum molestiae,
+                asperiores, in vitae voluptatibus quia doloremque expedita reiciendis sit itaque dolore. Ducimus odit
+                sapiente optio ad!</p>
+        </div>
+        <div class="service__cart2">
+            <img src="dist/img/service2" alt="">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A, praesentium maxime nobis laborum molestiae,
+                asperiores, in vitae voluptatibus quia doloremque expedita reiciendis sit itaque dolore. Ducimus odit
+                sapiente optio ad!</p>
+        </div>
+        <div class="service__cart3">
+            <img src="dist/img/service3" alt="">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. A, praesentium maxime nobis laborum molestiae,
+                asperiores, in vitae voluptatibus quia doloremque expedita reiciendis sit itaque dolore. Ducimus odit
+                sapiente optio ad!</p>
+        </div>
+    </div>
+
+    <div class="experience">
+        <p>28 | Year <br> of Experience</p>
+        <p>3.4k | Vehicle <br> Repaired</p>
+        <p>100% | Satisfied <br> Customers</p>
+    </div>
+    <div class="review">
+        <h1>Customers review</h1>
+        <img src="dist/img/star.png" alt="">
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae delectus porro omnis temporibus
+            assumenda! Fugit praesentium nostrum quo. Hic voluptas nulla corrupti voluptates eveniet aperiam, beatae
+            dicta facilis minus culpa?</p>
+        <p> <span> Hassan Barada,</span>Building contractor</p>
+    </div>
+
+    <div class="footer">
+        <h1>Contact Info</h1>
+        <ul>
+            <li>2605 Hill Road, Woodbridge, VA 22192</li>
+            <li>06 -99 44 95 57</li>
+            <li>officeone@youreemail.com</li>
+            <li>
+                <p> Monday- Fri:7:00AM - 6:00PM <br>
+                    Sturday: 9:00 AM - 5:00 PM <br>
+                    Sunday:Closed
+                </p>
+
+            </li>
+
+        </ul>
+    </div>
 
 </body>
 
